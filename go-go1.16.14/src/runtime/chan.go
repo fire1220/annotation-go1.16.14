829fa19c4f38ec2284d2282e5124aa21199e7efe
zhoujiandong <fire1220@live.cn>
2023-05-15 16:01:19 +0800

    管道备注

--- go-go1.16.14/src/runtime/chan.go
+++ go-go1.16.14/src/runtime/chan.go
@@ -163,7 +163,7 @@ func chansend1(c *hchan, elem unsafe.Pointer) {
  * been closed.  it is easiest to loop and re-run
  * the operation; we'll see that it's now closed.
  */
-// 注释：写入（发送）管道数据；ep是写入的数据变量指针(element pointer)， block是否阻塞
+// 注释：写入（发送）管道数据；ep是写入的数据变量指针(element pointer)， block是否阻塞，callerpc检查数据竞争时使用
 func chansend(c *hchan, ep unsafe.Pointer, block bool, callerpc uintptr) bool {
 	// 注释：如果管道没有初始化
 	if c == nil {

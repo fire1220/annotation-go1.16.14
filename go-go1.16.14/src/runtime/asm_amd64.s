ed885e6b6204e9850a1510e2e587bddc3d08138e
zhoujiandong <fire1220@live.cn>
2023-06-27 20:53:32 +0800

    调度循环-切换到系统堆栈

--- go-go1.16.14/src/runtime/asm_amd64.s
+++ go-go1.16.14/src/runtime/asm_amd64.s
@@ -381,7 +381,7 @@ TEXT runtime·systemstack(SB), NOSPLIT, $0-8
 	MOVQ	(g_sched+gobuf_sp)(DX), BX          // 注释：复制g.m.g0.sched.sp的参数到BX（SP存放函数参数访问头位置(基指针，栈低)）
 	// make it look like mstart called systemstack on g0, to stop traceback // 注释：下面是用汇编模拟函数调用
 	SUBQ	$8, BX                              // 注释：g0的栈(SP)扩容(扩容还没有被执行);BX -= 8; 结果是SP-=8
-	MOVQ	$runtime·mstart(SB), DX             // 注释：把运行的mstart指针赋值给寄存器DX【不知道为什么把mstart放到这里，不知道在哪里使用】
+	MOVQ	$runtime·mstart(SB), DX             // 注释：把运行的mstart指针赋值给寄存器DX【纳闷】【不知道为什么把mstart放到这里，不知道在哪里使用】
 	MOVQ	DX, 0(BX)                           // 注释：把mstart放到0(SP)位置（扩容后0(SP)是空指针;8(SP)是g0.gobuf_sp的指针）(下面会启动线程，并且启动调度系统)
 	MOVQ	BX, SP                              // 注释：（把g0的SP替换当前SP）g0.gobuf_sp切换到栈上，后面可以执行该栈(模拟函数调用)(下面启动线程，并且启动调度系统)
 

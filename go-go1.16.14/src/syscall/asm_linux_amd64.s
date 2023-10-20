39fb509a4ff374624cabbd2c62e95f41071e2833
zhoujiandong <fire1220@live.cn>
2023-10-20 18:53:34 +0800

    系统调用

--- go-go1.16.14/src/syscall/asm_linux_amd64.s
+++ go-go1.16.14/src/syscall/asm_linux_amd64.s
@@ -21,25 +21,25 @@
 // 注释：系统调用：第一个参数会放到AX中表示"系统调用编号"；第二个到第四个会放到DI，SI，DX中表示调的参数；返回值有三个分别是系统调用返回的两个参数和一个err
 TEXT ·Syscall(SB),NOSPLIT,$0-56
 	CALL	runtime·entersyscall(SB)
-	MOVQ	a1+8(FP), DI
-	MOVQ	a2+16(FP), SI
-	MOVQ	a3+24(FP), DX
-	MOVQ	trap+0(FP), AX	// syscall entry
-	SYSCALL
-	CMPQ	AX, $0xfffffffffffff001
-	JLS	ok
-	MOVQ	$-1, r1+32(FP)
-	MOVQ	$0, r2+40(FP)
-	NEGQ	AX
-	MOVQ	AX, err+48(FP)
+	MOVQ	a1+8(FP), DI                // 注释：函数入参的第2个参数放到DI寄存器里，DI是系统调用API的第一个参数
+	MOVQ	a2+16(FP), SI               // 注释：函数入参的第3个参数放到SI寄存器里，SI是系统调用API的第二个参数
+	MOVQ	a3+24(FP), DX               // 注释：函数入参的第4个参数放到DX寄存器里，DX是系统调用API的第三个参数
+	MOVQ	trap+0(FP), AX	            // 注释：函数入参的第1个参数放到AX寄存器里，AX是系统调用API存储“系统调用编号”的寄存器 // syscall entry
+	SYSCALL                             // 注释：执行系统调用
+	CMPQ	AX, $0xfffffffffffff001     // 注释：系统调用编号边界值比较（如果）
+	JLS	ok                              // 注释：系统调用编号边界值比较，如果小于边界值，则跳到ok标签处，否则继续向下执行
+	MOVQ	$-1, r1+32(FP)              // 注释：把-1放到第一个返回值里。（此处以后表示系统调用失败的处理）
+	MOVQ	$0, r2+40(FP)               // 注释：把0放到第二个返回值里
+	NEGQ	AX                          // 注释：把AX寄存器的值取反（AX = -AX）
+	MOVQ	AX, err+48(FP)              // 注释：把取反后的AX值放到第三个返回值里
 	CALL	runtime·exitsyscall(SB)
-	RET
-ok:
-	MOVQ	AX, r1+32(FP)
-	MOVQ	DX, r2+40(FP)
-	MOVQ	$0, err+48(FP)
+	RET                                 // 注释：函数返回
+ok:                                     // 注释：该标签表示系统调用成功
+	MOVQ	AX, r1+32(FP)               // 注释：把AX(系统调用的第1个返回值)的值放到第一个返回值里
+	MOVQ	DX, r2+40(FP)               // 注释：把DX(系统调用的第2个返回值)的值放到第二个返回值里
+	MOVQ	$0, err+48(FP)              // 注释：把0放到第三个返回值里
 	CALL	runtime·exitsyscall(SB)
-	RET
+	RET                                 // 注释：函数返回
 
 // func Syscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2, err uintptr)
 TEXT ·Syscall6(SB),NOSPLIT,$0-80

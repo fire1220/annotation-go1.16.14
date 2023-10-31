8fee5801c5d4c9350a9d78d06fd7c0f3318ebe37
zhoujiandong <fire1220@live.cn>
2023-10-31 11:39:18 +0800

    执行使用AES计算哈希值

--- go-go1.16.14/src/runtime/asm_amd64.s
+++ go-go1.16.14/src/runtime/asm_amd64.s
@@ -928,7 +928,7 @@ TEXT aeshashbody<>(SB),NOSPLIT,$0-0
 	// Fill an SSE register with our seeds. // 注释： 用我们的种子填写SSE登记册。
 	MOVQ	h+8(FP), X0			// 64 bits of per-table hash seed   // 注释：(就是上面的第二个参数)每个表的64位散列种子
 	PINSRW	$4, CX, X0			// 16 bits of length                // 注释：长度为16位,将CX中的低位插入到X0中的第4个位置
-	PSHUFHW $0, X0, X0			// repeat length 4 times total      // 注释：重复长度总计4次
+	PSHUFHW $0, X0, X0			// repeat length 4 times total      // 注释：重复长度总计4次，（根据X0（第三个参数）中编码对X0（第二个参数）高位进行打乱处理，结果放到0中（丢掉结果））
 	MOVO	X0, X1				// save unscrambled seed            // 注释：保存未处理的种子
 	PXOR	runtime·aeskeysched(SB), X0	// xor in per-process seed  // 注释：每个进程种子中的xor
 	AESENC	X0, X0				// scramble seed                    // 注释：打乱种子

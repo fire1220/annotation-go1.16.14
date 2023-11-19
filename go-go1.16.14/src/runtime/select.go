0ea06d490fd2547197f4138dc4a90f134e615498
zhoujiandong <fire1220@live.cn>
2023-11-19 22:47:58 +0800

    select...case大堆排序

--- go-go1.16.14/src/runtime/select.go
+++ go-go1.16.14/src/runtime/select.go
@@ -43,15 +43,20 @@ func sellock(scases []scase, lockorder []uint16) {
 	}
 }
 
+// 注释：设置case对应的管道解锁
 func selunlock(scases []scase, lockorder []uint16) {
 	// We must be very careful here to not touch sel after we have unlocked
 	// the last lock, because sel can be freed right after the last unlock.
+	// 注释：我们必须非常小心，不要在解锁最后一把锁后触摸sel，因为sel可以在最后一次解锁后立即释放。
 	// Consider the following situation.
 	// First M calls runtime·park() in runtime·selectgo() passing the sel.
+	// 注释：考虑以下情况。第一个M调用runtime·park（）在runtime·selectgo（）中传递sel。
 	// Once runtime·park() has unlocked the last lock, another M makes
 	// the G that calls select runnable again and schedules it for execution.
+	// 注释：一旦runtime·park（）解锁了最后一个锁，另一个M会使调用select的G再次可运行，并安排执行。
 	// When the G runs on another M, it locks all the locks and frees sel.
 	// Now if the first M touches sel, it will access freed memory.
+	// 注释：当G在另一个M上运行时，它锁定所有锁并释放sel。现在，如果第一个M触摸sel，它将访问释放的内存。
 	for i := len(lockorder) - 1; i >= 0; i-- {
 		c := scases[lockorder[i]].c
 		if i > 0 && c == scases[lockorder[i-1]].c {
@@ -271,23 +276,23 @@ func selectgo(cas0 *scase, order0 *uint16, pc0 *uintptr, nsends, nrecvs int, blo
 		cas = &scases[casi] // 注释：case的元素，当前下标对应的case
 		c = cas.c           // 注释：chan管道
 
-		if casi >= nsends {
-			sg = c.sendq.dequeue()
-			if sg != nil {
-				goto recv
+		if casi >= nsends { // 注释：(处理管道读操作)大于发送总数，代表case是接收管道
+			sg = c.sendq.dequeue() // 注释：到写入阻塞队列中取出数据，（接收管道，首先是看发送阻塞管道里是否有数据，如果有数据优先取出来）
+			if sg != nil {         // 注释：如果发送阻塞队列中有数据，则跳到接收位置处理接收管道数据
+				goto recv // 注释：跳到处理接收管道数据的代码位置
 			}
-			if c.qcount > 0 {
-				goto bufrecv
+			if c.qcount > 0 { // 如果走到这里代表阻塞队列中没有数据，并且管道里有数据，则说明管道缓冲区里有数据，跳到处理管道缓冲区位置处理读取数据
+				goto bufrecv // 注释：跳到处理管道缓冲区代码位置，读取管道数据
 			}
-			if c.closed != 0 {
-				goto rclose
+			if c.closed != 0 { // 注释：如果管道已经关闭则，跳到处理管道读取关闭时处理的函数位置
+				goto rclose // 注释：跳到度关闭管道代码位置
 			}
-		} else {
-			if raceenabled {
-				racereadpc(c.raceaddr(), casePC(casi), chansendpc)
+		} else { // 注释：处理管道写操作
+			if raceenabled { // 注释：判断是否开启数据竞争
+				racereadpc(c.raceaddr(), casePC(casi), chansendpc) // 注释：检测数据竞争
 			}
-			if c.closed != 0 {
-				goto sclose
+			if c.closed != 0 { // 注释：如果管道已经关闭，则不能向管道写入数据，跳到处理向已经关闭的管道写数据的代码位置，（其实就是panic）
+				goto sclose // 注释：跳到处理向已经关闭的管道写数据的代码位置
 			}
 			sg = c.recvq.dequeue()
 			if sg != nil {
@@ -429,7 +434,7 @@ func selectgo(cas0 *scase, order0 *uint16, pc0 *uintptr, nsends, nrecvs int, blo
 	selunlock(scases, lockorder)
 	goto retc
 
-bufrecv:
+bufrecv: // 注释：跳到处理管道缓冲区代码位置，读取管道数据
 	// can receive from buffer
 	if raceenabled {
 		if cas.elem != nil {
@@ -472,7 +477,7 @@ bufsend:
 	selunlock(scases, lockorder)
 	goto retc
 
-recv:
+recv: // 注释：处理接收管道数据的代码位置
 	// can receive from sleeping sender (sg)
 	recv(c, sg, cas.elem, func() { selunlock(scases, lockorder) }, 2)
 	if debugSelect {
@@ -481,7 +486,7 @@ recv:
 	recvOK = true
 	goto retc
 
-rclose:
+rclose: // 注释：跳到度关闭管道代码位置
 	// read at end of closed channel
 	selunlock(scases, lockorder)
 	recvOK = false
@@ -513,7 +518,7 @@ retc:
 	}
 	return casi, recvOK
 
-sclose:
+sclose: // 注释：处理向已经关闭的管道写数据(会触发panic)
 	// send on closed channel
 	selunlock(scases, lockorder)
 	panic(plainError("send on closed channel"))
